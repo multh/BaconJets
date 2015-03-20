7d9eaea9cc326d77e4678165a8a98183883474e0
Nataliia Kovalchuk <nataliia.kovalchuk@desy.de>
2015-03-20 10:14:36 +0100

    alpha & eta & pt bins

--- src/TestModule.cxx
+++ src/TestModule.cxx
@@ -58,7 +58,7 @@ TestModule::TestModule(Context & ctx) :
 
   eta_range = {0, 0.261, 0.522, 0.763, 0.957, 1.131, 1.305, 1.479, 1.93, 2.322, 2.411, 2.5, 2.853, 2.964, 3.139, 3.489, 5.191};
   pt_range = {66, 107, 191, 240, 306, 379, 468, 900};
-  alpha_range = {0.1, 0.125, 0.15, 0.175, 0.2, 0.225, 0.25};
+  alpha_range = {0., 0.1, 0.125, 0.15, 0.175, 0.2, 0.225, 0.25};
   // int size = sizeof(eta_range)/sizeof(double); // to get size of string object
   // eta_range.size() // to get size of vector
 
@@ -74,11 +74,18 @@ TestModule::TestModule(Context & ctx) :
     sprintf (eta_buffer, "%5.3f", eta_range[j]);
     eta_range_name.push_back(eta_buffer);
   }
-  cout << "eta range "<<eta_range_name[3]<< "pt range "<<pt_range_name[3]<< endl;
-
-  for( unsigned int j=0; j < eta_range.size()-1; ++j ){
-    for( unsigned int i=0; i < pt_range.size()-1; ++i ){
-        h_pt_bins.push_back(JECAnalysisHists(ctx,(std::string)("eta_"+eta_range_name[j]+"_"+eta_range_name[j+1]+"/pt_"+pt_range_name[i]+"_"+pt_range_name[i+1])));
+  std::vector<std::string> alpha_range_name;
+  for( unsigned int k=0; k < alpha_range.size(); ++k ){
+    char alpha_buffer [50];
+    sprintf (alpha_buffer, "%5.3f", alpha_range[k]);
+    alpha_range_name.push_back(alpha_buffer);
+  }
+  cout << "alpha range "<<alpha_range_name[3]<<"eta range "<<eta_range_name[3]<< "pt range "<<pt_range_name[3]<< endl;
+  for( unsigned int k=0; k < alpha_range.size()-1; ++k ){
+    for( unsigned int j=0; j < eta_range.size()-1; ++j ){
+        for( unsigned int i=0; i < pt_range.size()-1; ++i ){
+            h_pt_bins.push_back(JECAnalysisHists(ctx,(std::string)("alpha_"+alpha_range_name[k]+"_"+alpha_range_name[k+1]+"/eta_"+eta_range_name[j]+"_"+eta_range_name[j+1]+"/pt_"+pt_range_name[i]+"_"+pt_range_name[i+1])));
+        }
     }
   }
 //   for( unsigned int i=0; i < eta_range.size()-1; ++i ){
@@ -96,6 +103,8 @@ bool TestModule::process(Event & event) {
   const TClonesArray & js = event.get(h_jets);
   baconhep::TJet* jet1 = (baconhep::TJet*)js[0];
   baconhep::TJet* jet2 = (baconhep::TJet*)js[1];
+  Int_t njets = js.GetEntries();
+
 
  // std::cout <<"test module no corr: jet1->pt " << jet1->pt<< std::endl;
 
@@ -147,14 +156,24 @@ bool TestModule::process(Event & event) {
         probejet_pt = jet1->pt;
     }
   }
+  double alpha = 0.;
+  if (njets > 2) {
+    baconhep::TJet* jet3 = (baconhep::TJet*)js[2];
+        alpha = (2*(jet3->pt))/(jet1->pt + jet2->pt);
+        //cout << "alpha = "<< alpha << endl;
+  }
   //for alpha < 0.2
-  for( unsigned int j=0; j < eta_range.size()-1; ++j ){
-    if ((fabs(probejet_eta)>=eta_range[j])&&(fabs(probejet_eta)<eta_range[j+1])) {
-        for( unsigned int i=0; i < pt_range.size()-1; ++i ){
-            if ((probejet_pt>=pt_range[i])&&(probejet_pt<pt_range[i+1])) {
-                h_pt_bins[j*(pt_range.size()-1)+i].fill(event, ran);//j*pt_range.size()+i
-                //cout <<"eta range = "<< eta_range[j]<<" - "<< eta_range[j+1]<< "pt range = "<< pt_range[i]<<" - "<< pt_range[i+1]<<endl;
-                //cout <<"eta value = "<< fabs(probejet_eta) << " pt value = "<< probejet_pt <<endl;
+  for( unsigned int k=0; k < alpha_range.size()-1; ++k ){
+    if ((alpha>=alpha_range[k])&&(alpha<alpha_range[k+1])) {
+        for( unsigned int j=0; j < eta_range.size()-1; ++j ){
+            if ((fabs(probejet_eta)>=eta_range[j])&&(fabs(probejet_eta)<eta_range[j+1])) {
+                for( unsigned int i=0; i < pt_range.size()-1; ++i ){
+                    if ((probejet_pt>=pt_range[i])&&(probejet_pt<pt_range[i+1])) {
+                        h_pt_bins[k*(eta_range.size()-1)*(pt_range.size()-1)+j*(pt_range.size()-1)+i].fill(event, ran);//j*pt_range.size()+i
+                        //cout <<"eta range = "<< eta_range[j]<<" - "<< eta_range[j+1]<< "pt range = "<< pt_range[i]<<" - "<< pt_range[i+1]<<endl;
+                        //cout <<"eta value = "<< fabs(probejet_eta) << " pt value = "<< probejet_pt <<endl;
+                    }
+                }
             }
         }
     }
