ad1b97f998db5d0db6006daa667cef07942261dc
kovalch <nataliia.kovalchuk@desy.de>
2016-02-07 15:37:12 +0100

    up-to-date constant.h file for 2.11/fb cmssw74X

--- include/constants.h
+++ include/constants.h
@@ -8,13 +8,16 @@
 
 
 // static std::vector<double>   eta_range  = {0, 0.25, 0.5, 0.75, 1.0, 1.3, 1.6, 1.9, 2.2, 2.5, 2.7, 3.0, 3.2, 5.0};//{0, 1.3, 2.5, 3.0, 5.0};
-static std::vector<double>   eta_range  = {0, 0.261, 0.522, 0.783, 1.044, 1.305, 1.653, 1.93, 2.172, 2.322, 2.5, 2.65, 2.853, 2.964, 3.139, 5.191};
+static std::vector<double>   eta_range  = {0, 0.261, 0.522, 0.783, 1.044, 1.305, 1.653, 1.93, 2.172, 2.322, 2.5, 2.65, 2.853, 2.964, 3.139, 3.489, 5.191};
+// static std::vector<double>   eta_range  = {2.964, 3.139, 3.489, 5.191}; //for HF triggers use
+
 // static std::vector<double>   eta_range_mikko_barrel = {0, 0.8, 1.3};
 static std::vector<double>   eta_range_mikko  = {0, 1.3, 1.9, 2.5, 3.0, 3.2, 5.0, 0, 0.8, 1.3};
 
-static std::vector<double>   pt_range   = {55, 76, 93, 172, 232, 300, 366, 452, 558};
-
- 
+// static std::vector<double>   pt_range   = {55, 76, 93, 172, 232, 300, 366, 452, 558};
+static std::vector<double>   pt_range   = {56, 78, 100, 168, 232, 300, 366, 453, 562}; //2.11 nominal triggers
+// static std::vector<double>   pt_range   = {77, 131, 154, 244, 321, 426}; //2.11 HF triggers
+static int n_pt_bins = 9; //6 for HF; 9 for Nominal
 
 // static std::vector<double>   pt_range   = {43, 80, 88, 135, 223, 290, 365, 448, 561};
 // static std::vector<double>   pt_range   = {57, 80, 100, 166, 231, 290, 365, 478, 556};
@@ -61,16 +64,15 @@ static float s_n_Pv_z = 24.0;
 // static float s_Pt_Ave500_cut  = 561;
 
 /// Used for the 2015  40/pb  50ns data
-static float s_Pt_Ave40_cut   = 55;
-static float s_Pt_Ave60_cut   = 76;
-static float s_Pt_Ave80_cut   = 93;
-static float s_Pt_Ave140_cut  = 172;
-static float s_Pt_Ave200_cut  = 232;
-static float s_Pt_Ave260_cut  = 300;
-static float s_Pt_Ave320_cut  = 366;
-static float s_Pt_Ave400_cut  = 453;
-static float s_Pt_Ave500_cut  = 558;
-static int n_pt_bins = 9;
+// static float s_Pt_Ave40_cut   = 55;
+// static float s_Pt_Ave60_cut   = 76;
+// static float s_Pt_Ave80_cut   = 93;
+// static float s_Pt_Ave140_cut  = 172;
+// static float s_Pt_Ave200_cut  = 232;
+// static float s_Pt_Ave260_cut  = 300;
+// static float s_Pt_Ave320_cut  = 366;
+// static float s_Pt_Ave400_cut  = 453;
+// static float s_Pt_Ave500_cut  = 558;
 
 /// Used for the 2015  ~1/fb  25ns data
 // static float s_Pt_Ave40_cut   = 55;
@@ -83,13 +85,33 @@ static int n_pt_bins = 9;
 // static float s_Pt_Ave400_cut  = 451;
 // static float s_Pt_Ave500_cut  = 561;
 
+/// Used for the 2015  2.11/fb  25ns data
+static float s_Pt_Ave40_cut   = 56;
+static float s_Pt_Ave60_cut   = 78;
+static float s_Pt_Ave80_cut   = 100;
+static float s_Pt_Ave140_cut  = 168;
+static float s_Pt_Ave200_cut  = 232;
+static float s_Pt_Ave260_cut  = 300;
+static float s_Pt_Ave320_cut  = 366;
+static float s_Pt_Ave400_cut  = 453;
+static float s_Pt_Ave500_cut  = 562;
+
+/// HF thresholds Used for the 2015  2.11/fb  25ns data
+static float s_Pt_Ave60HF_cut   = 77;
+static float s_Pt_Ave80HF_cut   = 131;
+static float s_Pt_Ave100HF_cut  = 154;
+static float s_Pt_Ave160HF_cut  = 244;
+static float s_Pt_Ave220HF_cut  = 321;
+static float s_Pt_Ave300HF_cut  = 426;
+
+
 /// HF thresholds Used for the 2015  40/pb  50ns data
-static float s_Pt_Ave60HF_cut   = 76;
-static float s_Pt_Ave80HF_cut   = 93;
-static float s_Pt_Ave100HF_cut  = 120;
-static float s_Pt_Ave160HF_cut  = 186;
-static float s_Pt_Ave220HF_cut  = 253;
-static float s_Pt_Ave300HF_cut  = 342;
+// static float s_Pt_Ave60HF_cut   = 76;
+// static float s_Pt_Ave80HF_cut   = 93;
+// static float s_Pt_Ave100HF_cut  = 120;
+// static float s_Pt_Ave160HF_cut  = 186;
+// static float s_Pt_Ave220HF_cut  = 253;
+// static float s_Pt_Ave300HF_cut  = 342;
 
 /// HF thresholds Used for the 2015  ~1/fb  25ns data
 // static float s_Pt_Ave60HF_cut   = 77;
@@ -121,30 +143,61 @@ static float c_err_nominal[n] = {0.023, 0.029, 0.030, 0.094, 0.123, 0.111, 0.286
 /** \brief Dijet event weighting **/
 // in each pt-region defined by the trigger thresholds # of events in MC and in data should equal
 
-///Asympt Mc
-static float scale_factor_noJERA[] = {3153, 11221.4, 10024.6, 9616.32, 465256, 667300, 4.138e+06, 1.43267e+07, 1.69729e+07};//run2 69000 V6 Asym
-static float scale_factor_centrA[] = {3001.18, 10864.1, 9891.95, 9607.91, 464409, 667504, 4.1411e+06, 1.43601e+07, 1.69549e+07};//run2 69000 V6, cent.smear Asym
-static float scale_factor_upA[] = {2896.24, 10658.4, 9750.38, 9567.18, 462895, 665397, 4.1283e+06, 1.43305e+07, 1.6918e+07};//run2 69000 V6, up.smear Asym
-static float scale_factor_downA[] = {3097.2, 11091.8, 9998.62, 9635.96, 466336, 669405, 4.15346e+06, 1.43912e+07, 1.6992e+07};//run2 69000 V6, down.smear Asym
-static float scale_factor_centrA_PU80[] = {3415.88, 11734.9, 10368.5, 10024.6, 483457, 693496, 4.28772e+06, 1.49127e+07, 1.76746e+07};//run2 80000 V6, cent.smear Asym
 
-static float scale_factor_centrA_PU69_set1[] = {1119.55, 3035.73, 2778.49, 2689.78, 69284.6, 99612, 612942, 1.89493e+06, 1.83119e+06};//69mb V6, cent. Asym set1
-static float scale_factor_centrA_PU69_set2[] = {424.986, 1632.72, 1493.05, 1433.51, 63826.9, 91486.2, 542606, 1.97444e+06, 1.9212e+06};//69mb V6, cent. Asym set2
-static float scale_factor_centrA_PU69_set3[] = {383.799, 1531.58, 1362.94, 1349.3, 58697.6, 87290.4, 519621, 2.02935e+06, 1.9623e+06};//69mb V6, cent. Asym set3
-static float scale_factor_centrA_PU69_set4[] = {302.178, 1239.91, 1109.87, 1082.25, 52135.2, 74471.4, 448186, 1.77749e+06, 1.72852e+06};//69mb V6, cent. Asym set4
-static float scale_factor_centrA_PU69_set5[] = {190.486, 817.24, 742.009, 760.26, 45077.2, 65552.7, 405774, 1.50184e+06, 1.74933e+06};//69mb V6, cent. Asym set5
-static float scale_factor_centrA_PU69_set6[] = {113.783, 537.28, 497.731, 477.569, 40197.2, 57013.3, 377918, 1.14542e+06, 1.84246e+06};//69mb V6, cent. Asym set6
-static float scale_factor_centrA_PU69_set7[] = {260.369, 1026.12, 971.377, 927.87, 49901.1, 69802.7, 435178, 1.3947e+06, 1.6765e+06};//69mb V6, cent. Asym set7
-static float scale_factor_centrA_PU69_set8[] = {127.443, 601.259, 542.876, 510.048, 46458.6, 66212.6, 435145, 1.46857e+06, 2.16278e+06};//69mb V6, cent. Asym set8
-static float scale_factor_centrA_PU69_set9[] = {78.5873, 442.229, 393.594, 377.323, 38830.2, 56062.8, 363732, 1.17338e+06, 2.08058e+06};//69mb V6, cent. Asym set9
+/** \brief pt-bins corresponds for the 2015  2.11/fb  25ns data **/
+///Asympt MC
+static float scale_factor_noJERA[] = {3134.9, 10969, 10001.3, 27387.8, 468903, 672528, 4.17038e+06, 1.44398e+07, 1.7089e+07};//run2 69000 V6 Asym 60for80only 2
+static float scale_factor_centrA[] = {2981.98,10586.7,9820.56,27160.5,464409,667504,4.1411e+06,1.4364e+07,1.69089e+07};//run2 69000 V6, cent.smear Asym 60for80only 2
+static float scale_factor_upA[] = {2874.51, 10396.2, 9712.4, 27018.3, 462895, 665397, 4.1283e+06, 1.43384e+07, 1.68405e+07};//run2 69000 V6, up.smear Asym 60for80only 2
+static float scale_factor_downA[] = {3075.04, 10785.2, 9907.87, 27249.6, 466336, 669405, 4.15346e+06, 1.43899e+07, 1.69864e+07};//run2 69000 V6, down.smear Asym 60for80only 2
+static float scale_factor_centrA_PU80[] = {3370.87, 11363.8, 10277, 28299, 483457, 693496, 4.28772e+06, 1.4921e+07, 1.76017e+07};//run2 80000 V6, cent.smear Asym 60for80only 2
+// static float scale_factor_centrA[] = {656.891,795.412,2805.58,50533.7,117209,22367.6};//run2 69000 V6, cent.smear Asym for HF
+
+static float scale_factor_centrA_PU69_set1[] = {1111.78, 2969.16, 2753.22, 3888.24, 69284.6, 99612, 612942, 1.89654e+06, 1.81834e+06};//69mb V6, cent. Asym set1
+static float scale_factor_centrA_PU69_set2[] = {423.303, 1591.85, 1484.52, 2697.94,  63826.9, 91486.2, 542606, 1.97481e+06, 1.91547e+06};//69mb V6, cent. Asym set2
+static float scale_factor_centrA_PU69_set3[] = {379.129, 1482.2, 1349.83, 2584.28, 58697.6, 87290.4, 519621, 2.02811e+06, 1.96579e+06};//69mb V6, cent. Asym set3
+static float scale_factor_centrA_PU69_set4[] = {299.906, 1204.09, 1100, 2338.59, 52135.2, 74471.4, 448186, 1.7772e+06, 1.72785e+06};//69mb V6, cent. Asym set4
+static float scale_factor_centrA_PU69_set5[] = {189.08, 793.597, 736.411, 1947.83, 45077.2, 65552.7, 405774, 1.50256e+06, 1.74423e+06};//69mb V6, cent. Asym set5
+static float scale_factor_centrA_PU69_set6[] = {113.619, 525.601, 494.17, 2271.52, 40197.2, 57013.3, 377918, 1.14571e+06, 1.83863e+06};//69mb V6, cent. Asym set6
+static float scale_factor_centrA_PU69_set7[] = {260.853, 1005.81, 970.333, 4190.05, 49901.1, 69802.7, 435178, 1.39686e+06, 1.66173e+06};//69mb V6, cent. Asym set7
+static float scale_factor_centrA_PU69_set8[] = {125.985, 587.013, 536.037, 4058.73, 46458.6, 66212.6, 435145, 1.46986e+06, 2.15342e+06};//69mb V6, cent. Asym set8
+static float scale_factor_centrA_PU69_set9[] = {78.3287, 427.348, 396.04, 3183.31, 38830.2, 56062.8, 363732, 1.17238e+06, 2.0834e+06};//69mb V6, cent. Asym set9
+
 
 
 ///Flat MC
-static float scale_factor_noJERF[] = {2543.11, 9228.77, 8404.76, 7916.86, 385342, 550228, 3.42556e+06, 1.17561e+07, 1.41737e+07};//run2 69000 V6 Flat
-static float scale_factor_centrF[] = {2413.77, 8916.56, 8200.09, 7844.02, 382407, 544738, 3.39795e+06, 1.17032e+07, 1.40901e+07};//run2 69000 V6, cent.smear Flat
+static float scale_factor_noJERF[] = {2520.98, 9041.37, 8321.97, 22310.1, 385342, 550228, 3.42556e+06, 1.17565e+07, 1.41959e+07};//run2 69000 V6 Flat 60for80only 2
+static float scale_factor_centrF[] = {2393.91, 8738.02, 8138.46, 22131.8, 382407, 544738, 3.39795e+06, 1.17061e+07, 1.40886e+07};//run2 69000 V6, cent.smear Flat 60for80only 2
 
 
 
+/** \brief pt-bins corresponds for the 2015  40/pb  50ns data **/
+///Asympt MC
+// static float scale_factor_noJERA[] = {3153, 11221.4, 10024.6, 9616.32, 465256, 667300, 4.138e+06, 1.43267e+07, 1.69729e+07};//run2 69000 V6 Asym
+// static float scale_factor_centrA[] = {3001.18, 10864.1, 9891.95, 9607.91, 464409, 667504, 4.1411e+06, 1.43601e+07, 1.69549e+07};//run2 69000 V6, cent.smear Asym
+// static float scale_factor_upA[] = {2896.24, 10658.4, 9750.38, 9567.18, 462895, 665397, 4.1283e+06, 1.43305e+07, 1.6918e+07};//run2 69000 V6, up.smear Asym
+// static float scale_factor_downA[] = {3097.2, 11091.8, 9998.62, 9635.96, 466336, 669405, 4.15346e+06, 1.43912e+07, 1.6992e+07};//run2 69000 V6, down.smear Asym
+// static float scale_factor_centrA_PU80[] = {3415.88, 11734.9, 10368.5, 10024.6, 483457, 693496, 4.28772e+06, 1.49127e+07, 1.76746e+07};//run2 80000 V6, cent.smear Asym
+
+// static float scale_factor_centrA_PU69_set1[] = {1119.55, 3035.73, 2778.49, 2689.78, 69284.6, 99612, 612942, 1.89493e+06, 1.83119e+06};//69mb V6, cent. Asym set1
+// static float scale_factor_centrA_PU69_set2[] = {424.986, 1632.72, 1493.05, 1433.51, 63826.9, 91486.2, 542606, 1.97444e+06, 1.9212e+06};//69mb V6, cent. Asym set2
+// static float scale_factor_centrA_PU69_set3[] = {383.799, 1531.58, 1362.94, 1349.3, 58697.6, 87290.4, 519621, 2.02935e+06, 1.9623e+06};//69mb V6, cent. Asym set3
+// static float scale_factor_centrA_PU69_set4[] = {302.178, 1239.91, 1109.87, 1082.25, 52135.2, 74471.4, 448186, 1.77749e+06, 1.72852e+06};//69mb V6, cent. Asym set4
+// static float scale_factor_centrA_PU69_set5[] = {190.486, 817.24, 742.009, 760.26, 45077.2, 65552.7, 405774, 1.50184e+06, 1.74933e+06};//69mb V6, cent. Asym set5
+// static float scale_factor_centrA_PU69_set6[] = {113.783, 537.28, 497.731, 477.569, 40197.2, 57013.3, 377918, 1.14542e+06, 1.84246e+06};//69mb V6, cent. Asym set6
+// static float scale_factor_centrA_PU69_set7[] = {260.369, 1026.12, 971.377, 927.87, 49901.1, 69802.7, 435178, 1.3947e+06, 1.6765e+06};//69mb V6, cent. Asym set7
+// static float scale_factor_centrA_PU69_set8[] = {127.443, 601.259, 542.876, 510.048, 46458.6, 66212.6, 435145, 1.46857e+06, 2.16278e+06};//69mb V6, cent. Asym set8
+// static float scale_factor_centrA_PU69_set9[] = {78.5873, 442.229, 393.594, 377.323, 38830.2, 56062.8, 363732, 1.17338e+06, 2.08058e+06};//69mb V6, cent. Asym set9
+
+
+
+///Flat MC
+
+// static float scale_factor_noJERF[] = {2543.11, 9228.77, 8404.76, 7916.86, 385342, 550228, 3.42556e+06, 1.17561e+07, 1.41737e+07};//run2 69000 V6 Flat
+// static float scale_factor_centrF[] = {2413.77, 8916.56, 8200.09, 7844.02, 382407, 544738, 3.39795e+06, 1.17032e+07, 1.40901e+07};//run2 69000 V6, cent.smear Flat
+
+
+///For RunI
 //static float scale_factor1[] = {2.14563, 4.27422,7.57198,  10.7035,17.899, 15.6095, 4.64629};//run1
 // static float scale_factor2[] = {242.617, 3514.77, 49549.8, 180540, 693177, 1.83449e+06, 1.63241e+07};//run1
 
